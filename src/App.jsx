9df766ef1166f079fe3ec4595fc9680805c92690
Moses <dmosesfernando@gmail.com>
2025-07-10 16:40:01 +0530

    Initialize React + Vite project with ESLint setup
    Add initial project files for a React application using Vite, including ESLint configuration, basic source files, and documentation. Project dependencies and configuration files are included to support development and linting.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+      <p>This is a simple React application.</p>
+    </div>
+  )
+}
+
+export default App
