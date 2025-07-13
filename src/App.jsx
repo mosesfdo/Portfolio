2fefed21c0bf6f0877edb70ce662abe56b9f9c2a
Moses <dmosesfernando@gmail.com>
2025-07-13 13:34:12 +0530

    hero and proect section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import './App.css'
 import heroimage from './assets/Images/anime-me.png'
 import HeroText from './assets/Components/HeroText/HeroText'
 import ProjectCard from './assets/Components/ProjectCard/ProjectCard'
-import tempmailimg from './assets/Images/tempmail-scrn.png'
-import tunegrabimg from './assets/Images/tunegrab-scrn.png'
-import reactportfolioimg from './assets/Images/react-portfolio.png'
 
 const App = () => {
   return (
@@ -20,34 +17,7 @@ const App = () => {
       </section>
 
       <section id="projects" className="projects-section wrapper">
-        <div className="row-one">
-          <ProjectCard 
-          projectImage={tempmailimg} 
-          overlayContent="TempMail" 
-          description={<p>This is a temporary email service.</p>}
-          />
-          <ProjectCard 
-          projectImage={reactportfolioimg} 
-          overlayContent="React Portfolio" 
-          description={<p>This is a portfolio website built with React.</p>} 
-          width="fit-content"
-          height="min-content"
-          />
-        </div>
-
-        <div className="row-two">
-          <ProjectCard 
-          projectImage={tunegrabimg} 
-          overlayContent="TuneGrab" 
-          description={<p>TuneGrab is a music download application.</p>} 
-          width="fit-content"
-          height="300px"
-          />
-        </div>
-      </section>
-
-      <section id="about" className="about-section wrapper">
-
+        <ProjectCard/>
       </section>
 
     </div>
--- src/assets/Components/ProjectCard/ProjectCard.jsx
+++ src/assets/Components/ProjectCard/ProjectCard.jsx
@@ -1,19 +1,43 @@
-
 import React from 'react'
 import './ProjectCard.css'
+import tempmailimg from '../../../assets/Images/tempmail-scrn.png'
+import tunegrabimg from '../../../assets/Images/tunegrab-scrn.png'
+import reactportfolioimg from '../../../assets/Images/react-portfolio.jpeg'
 
-const ProjectCard = ({ projectImage, overlayContent, description, customWidth }) => {
-  const cardStyle = {
-    width: customWidth || '400px',
-  };
+const ProjectCard = () => {
   return (
-    <div className="project-card" style={cardStyle}>
-      <div className="project-image-container">
-        <img className="project-image" src={projectImage} alt="Project" />
-        <div className="project-overlay">
-          <div className="project-overlay-content">
-            <div className="project-title">{overlayContent}</div>
-            {description && <div className="project-description">{description}</div>}
+    <div className="project-card">
+      <div className="row-one">
+        <div className="project-card-image-container projectcard">
+          <img src={tempmailimg} alt="TempMail" className="project-image tempmail" />
+          <div className="project-card-gradient-overlay"></div>
+          <div className="project-card-overlay-content">
+            <h3 className="project-card-title">TempMail | <a href="https://github.com/Moses-01/TempMail">View Project </a></h3>
+            <p>
+              TempMail is a learning-focused temporary email generator
+               built with HTML, CSS, and vanilla JavaScript.
+            </p>
+          </div>
+        </div>
+        <div className="project-card-image-container projectcard">
+          <img src={reactportfolioimg} alt="React Portfolio" className="project-image react-portfolio" />
+          <div className="project-card-gradient-overlay"></div>
+          <div className="project-card-overlay-content">
+            <h3 className="project-card-title">React Portfolio | <a href="https://github.com/Moses-01/React-Portfolio">View Project</a></h3>
+            <p>A personal portfolio website built with React to show
+               case my projects and skills as a developer.</p>
+          </div>
+        </div>
+      </div>
+      <p className='project-content'>"These projects were developed either during my learning process or as part of college coursework."</p>
+
+      <div className="row-two">
+        <div className="project-card-image-container projectcard">
+          <img src={tunegrabimg} alt="TuneGrab" className="project-image tunegrab" />
+          <div className="project-card-gradient-overlay"></div>
+          <div className="project-card-overlay-content">
+            <h3 className="project-card-title">TuneGrab | <a href="https://github.com/Moses-01/TuneGrab">View Project</a></h3>
+            <p>TuneGrab  uses CustomTkinter for the interface, integrates with the Spotify Web API, and handles audio downloads and conversions using yt-dlp and FFmpeg.</p>
           </div>
         </div>
       </div>
